6b209af72d38c3b4d70d1c8f7b5fc2b2babd927f
Biswajit Ghosh <biswajitg469@gmail.com>
2025-11-05 16:08:23 +0530

    Removed node_modules from tracking

---
+++ src/components/aboutme/journey/EducationJourney.js
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import SchoolIcon from "@mui/icons-material/School";
+import { Link } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import Flip from "react-reveal/Flip";
+
+function EducationJourney() {
+  return (
+    <div className="mt-5">
+      <div>
+        <Link className="btn btn-primary m-1" to="/educationjourney">
+          Educational Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/experiencejourney">
+          Experience Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/technicaljourney">
+          Technical Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/projectjourney">
+          Project Journey
+        </Link>
+      </div>
+      <div className="mt-5">
+        <Flip top cascade>
+          <h1>Qualification Journey</h1>
+        </Flip>
+      </div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2005 - 2015"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            SECONDARY SCHOOL CERTIFICATE
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            Suri Netaji Vidyabhaban, Suri, West Bengal
+
+          </h4>
+          <p>
+            Studied here from Class 1 to 10th as a part of secondary school
+            Certificate and passed the exam with 58.4% marks.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2017 - 2020"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">
+            DIPLOMA IN ENGINEERING
+          </h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+           Bengal Institute of Polytechnic , Sagar, West Bengal
+          </h4>
+          <p>
+            Studied Diploma in Computer Science Technology from Bengal Institute of Polytechnic and gain CGPA 6.85.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date="2019 - 2023"
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<SchoolIcon />}
+        >
+          <h4 className="vertical-timeline-element-title">GRADUATION</h4>
+          <h4 className="vertical-timeline-element-subtitle mt-2">
+            Birbhum Institute of Engineering & Technology, Kamalpur, West Bengal
+
+          </h4>
+          <p>
+            Studied Bachelor of Technology degree in Computer Science and
+            Engineering field from Birbhum Institute of Engineering & Technology and gain CGPA 7.51.
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          iconStyle={{ background: "#fbd9ad", color: "rgb(78 22 112)" }}
+          icon={<StarIcon />}
+        />
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default EducationJourney;
